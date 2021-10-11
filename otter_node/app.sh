050ff0f061467effccb334e219fd8a9d5dd25f7c
lsqtzj <lsqtzj@msn.com>
2021-10-11 09:13:54 +0200

    解决数据保存问题</br> 使用方法：https://blog.csdn.net/lsqtzj/article/details/120609101

--- otter_node/app.sh
+++ otter_node/app.sh
@@ -217,11 +217,12 @@ function stop_node() {
 
 function start_mysql() {    
     # start mysql
+            
+    cmd="sed -i -e 's/^server-id.*$/server-id=${ZOO_MY_ID:-1}/' /etc/my.cnf"
+    eval $cmd
     if [ -z "$(ls -A /var/lib/mysql)" ]; then
         echo "start modify mysql password ..."
         cmd="sed -i -e  '2a skip-grant-tables' /etc/my.cnf"
-        eval $cmd
-        cmd="sed -i -e  '2a server-id=${ZOO_MY_ID:-1}' /etc/my.cnf"
         eval $cmd
         /usr/sbin/mysqld --user=mysql --datadir=/var/lib/mysql --initialize 1>>/tmp/start.log 2>&1
         service mysqld start
